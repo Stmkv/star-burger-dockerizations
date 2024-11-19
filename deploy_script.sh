f62dafe483fe116ded8f514f886ea00ba0a4e0d2
Макаров Степан Александрович <st.mkv03@gmail.com>
2024-11-20 00:34:36 +0300

    refactor(deploy_script)

--- deploy_script.sh
+++ deploy_script.sh
@@ -5,8 +5,9 @@ docker-compose down
 
 cd /app/
 
-git pull origin master
+git pull origin main
 
 docker-compose up -d
-
+docker-compose exec app_web python manage.py migrate
+docker-compose exec app_web python manage.py collectstatic
 echo "Обновление завершено!"
